62206c8ffd1c4c80b210e33c6f6b02bc39dd56a2
SuperPhone <zero5330@gmail.com>
2020-06-28 11:54:31 +0800

    add create_tags.sh

---
+++ create_tags.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+function usage()
+{
+    echo
+    echo "Usage:"
+    echo "  $(basename $0) kernel"
+    echo
+}
+
+tag_type=$1
+
+if [ "$tag_type" == "kernel" ]; then
+    rm -rf gtags.files
+    find . -type d                  \
+        \(                          \
+        -path ./.git -o             \
+        -path ./Documentation       \
+        \)
+        -prune -o                   \
+        -type f                     \
+        \(                          \
+        -name "*.[chxsS]" -o        \
+        -name "*.cpp"     -o        \
+        -name ".dts"                \
+        \)
+        -print > gatags.files
+
+        echo "Create kernel tags"
+fi
+
+export GTAGSFORCECPP=1
+
+#create tags
+if [ -f ./gtags.files ]; then
+    gtags -f ./gtags.files
+else
+    gtags
+fi
+
